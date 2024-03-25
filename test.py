091922ed5dffc788190cb0e7dd5995fa98efacc8
Your Name <you@example.com>
2024-03-25 16:38:53 -0400

    Changed too much to say it all in one message

--- test.py
+++ test.py
@@ -1,60 +1,14 @@
-import requests
-#from simple_term_menu import TerminalMenu
+from pick import pick
+liste = ["A", "B", "C"]
 
-url = "https://host-hifi-restapi-on-vercel-git-main-sachinsenal0x64.vercel.app/"
-url = "https://tidal.401658.com/"
+choices = pick(
+    liste,
+    title=(
+        f"search.\n"
+        "Press SPACE to select, RETURN to download, CTRL-C to exit."
+    ),
+    multiselect=True,
+    min_selection_count=1,
+)
 
-
-query = "Starships"
-def preview_content():
-    return f"{liste[song]['title']}"
-
-querystring = {"q":query}
-response = 0
-while type(response) != list:
-    try:
-        response = requests.get(f"{url}search/", params=querystring).json()
-    except:
-        pass
-
-liste = []
-for part in range(len(response)):
-    joined = {}
-    joined.update(response[part])
-    liste.append(joined)
-
-summary = []
-
-for song in range(len(liste)):
-    summary.append(f"{song + 1}. {liste[song]['title']} by {liste[song]['artist']['name']} [{liste[song]['id']}]")
-
-#terminal_menu = TerminalMenu(
-#    summary,
-#    title=(f"Results for track '{query}' from Tidal\nSPACE - select, ENTER - download, ESC - exit"),
-#    preview_command="bat --color=always code/tidal.py",
-#    preview_size=0.75,
-#    cycle_cursor=True,
-#    clear_screen=True,
-#    multi_select=True,
-#    multi_select_select_on_accept=False,
-#)
-#
-#chosen = terminal_menu.show()
-    
-
-#if chosen is None:
-#    print("Nothing chosen, exiting..")
-#else:
-#    for song in range(chosen):
-#        with open(f"{liste[song]['artist']['name']} - {liste[song]['title']}.flac", "wb") as f:
-#            f.write(requests.get(id_quality_song(liste[song]["id"], "HI_RES_LOSSLESS").content))
-
-
-
-#print(response)
-#print("\n\n\n\n", joined)
-#print(joined["OriginalTrackUrl"])
-#print(requests.get(joined["OriginalTrackUrl"]))
-
-#with open("file.flac", "wb") as f:
-#    f.write(requests.get(joined["OriginalTrackUrl"]).content)
+#DOESNT WORK WTF I HATE THIS
--- tidal.py
+++ tidal.py
@@ -1,22 +1,98 @@
 import requests
 import os
+from pick import pick
+from simple_term_menu import TerminalMenu
 
-url = "https://tidal.401658.xyz"
+
 url = "https://host-hifi-restapi-on-vercel-git-main-sachinsenal0x64.vercel.app"
+url = "https://tidal.401658.xyz"
 qualities = ["HI_RES_LOSSLESS", "HI_RES", "LOSSLESS", "HIGH", "LOW"]
 
 def query_song(query):
+
+    query = "Starships"
+    def preview_content():
+        return f"{liste[song]['title']}"
+
     querystring = {"q":query}
     response = 0
     while type(response) != list:
         try:
-            response = requests.get(f"{url}/search/", params=querystring).json()
+            response = requests.get(f"{url}search/", params=querystring).json()
         except:
             pass
 
-    joined = {}
-    for parts in response:
-        joined.update(parts)
+    liste = []
+    for part in range(len(response)):
+        joined = {}
+        joined.update(response[part])
+        liste.append(joined)
+
+    summary = []
+
+    for song in range(len(liste)):
+        summary.append(f"{song + 1}. {liste[song]['title']} by {liste[song]['artist']['name']} [{liste[song]['id']}]")
+
+    print(summary)
+
+    #if os.name == "nt":
+    #    from pick import pick
+    #
+    #    choices = pick(
+    #        summary,
+    #        title=(
+    #            f"Results for track '{query}' from Tidal\nSPACE - select, ENTER - download, ESC - exit"
+    #        ),
+    #        min_selection_count=1,
+    #        multiselect=True,
+    #    )
+    #
+    #    choices()
+    #    print(choices)
+
+
+    title = 'Please choose your favorite programming language (press SPACE to mark, ENTER to continue): '
+    options = ['Java', 'JavaScript', 'Python', 'PHP', 'C++', 'Erlang', 'Haskell']
+    option, index = pick(options, title)
+    print(option)
+    print(index)
+    print("\n\n\n")
+
+
+
+
+
+
+    #terminal_menu = TerminalMenu(
+    #    summary,
+    #    title=(f"Results for track '{query}' from Tidal\nSPACE - select, ENTER - download, ESC - exit"),
+    #    preview_command="bat --color=always code/tidal.py",
+    #    preview_size=0.75,
+    #    cycle_cursor=True,
+    #    clear_screen=True,
+    #    multi_select=True,
+    #    multi_select_select_on_accept=False,
+    #)
+    #
+    #chosen = terminal_menu.show()
+
+
+    #if chosen is None:
+    #    print("Nothing chosen, exiting..")
+    #else:
+    #    for song in range(chosen):
+    #        with open(f"{liste[song]['artist']['name']} - {liste[song]['title']}.flac", "wb") as f:
+    #            f.write(requests.get(id_quality_song(liste[song]["id"], "HI_RES_LOSSLESS").content))
+
+
+
+    #print(response)
+    #print("\n\n\n\n", joined)
+    #print(joined["OriginalTrackUrl"])
+    #print(requests.get(joined["OriginalTrackUrl"]))
+
+    #with open("file.flac", "wb") as f:
+    #    f.write(requests.get(joined["OriginalTrackUrl"]).content)
     
 
 def query_quality_song(query, quality):
