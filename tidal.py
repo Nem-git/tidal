083de9b66c2c495245076eddca1d29e595fd74c6
Your Name <you@example.com>
2024-04-08 18:27:13 -0400

    a

--- tidal.py
+++ tidal.py
@@ -11,9 +11,14 @@ def String_replace(old):
     old.replace("\\", "")
     return old
 
+def Validate_request(request):
+    response = requests.get(request)
+    while response.status_code != 200:
+        response = requests.get(request)
+    return response.json()
 
 def Download_song(song_id, song_quality, artist_name, release_year):
-    song_response = requests.get(f"{url}track/?id={song_id}&quality={song_quality}").json()
+    song_response = Validate_request(f"{url}track/?id={song_id}&quality={song_quality}")
     song_path = f"../{artist_name}/{artist_name} - {song_response[0]['album']['title']} ({release_year})/"
 
     if not os.path.exists(String_replace(song_path)):
@@ -22,9 +27,7 @@ def Download_song(song_id, song_quality, artist_name, release_year):
         "Something doesnt work with the path"
     
     with open(f"{song_path}{artist_name} - {song_response[0]['title']}.flac", "wb") as f:
-        song_link = requests.get(song_response[2]['OriginalTrackUrl'])
-        while 200 != song_link.status_code:
-            song_link = requests.get(song_response[2]['OriginalTrackUrl'])
+        song_link = Validate_request(song_response[2]['OriginalTrackUrl'])
         f.write(song_link.content)
     Download_cover(song_response[0]['album']['cover'], song_path)
 
@@ -38,9 +41,7 @@ def Download_cover(cover_code, cover_path):
         cover_url += f"{i}/"
 
     with open(f"{cover_path}cover.jpg", "wb") as c:
-        cover_response = requests.get(f"{cover_url}640x640.jpg")
-        while 200 != cover_response.status_code:
-            cover_response = requests.get(f"{cover_url}640x640.jpg")
+        cover_response = Validate_request(f"{cover_url}640x640.jpg")
         c.write(cover_response.content)
 
 
@@ -48,18 +49,18 @@ os.system("cls")
 while True:
     print("Download Artist ONLY(fuck you)\n")
     artist_name = String_replace(input("Artist Name: "))
-    artist_response = requests.get(f"{url}search/?a={artist_name}").json()
+    artist_response = Validate_request(f"{url}search/?a={artist_name}")
 
     for artist in artist_response[0]['artists']['items']:
         print(f"{artist['id']} - {artist['name']}")
     
     artist_id = input("Artist ID: ")
     os.system("cls")
-    artist_id_response = requests.get(f"{url}search/?f={artist_id}").json()
+    artist_id_response = Validate_request(f"{url}search/?f={artist_id}")
 
     for album in artist_id_response[1]:
         print(f"{album['id']}. {album['album']['title']} - {album['title']}")
         artist_name = artist_id_response[0]['rows'][0]['modules'][0]['pagedList']['items'][0]['artists'][0]['name']
         release_year = album['album']['releaseDate'].split("-")[0]
-        Download_song(album['id'], "LOW", artist_name, release_year)
+        Download_song(album['id'], "HI_RES_LOSSLESS", artist_name, release_year)
         
