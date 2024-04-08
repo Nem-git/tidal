25b1487d1ee1bc70f9b965654e3f5d8d83e085bb
Your Name <you@example.com>
2024-04-08 14:09:21 -0400

    made stuff

--- tidal.py
+++ tidal.py
@@ -5,12 +5,19 @@ import os
 url = "https://host-hifi-restapi-on-vercel-git-main-sachinsenal0x64.vercel.app/"
 url = "https://tidal.401658.xyz/"
 
+def String_replace(old):
+    old.replace("'", "")
+    old.replace('"', "")
+    old.replace("\\", "")
+    return old
+
+
 def Download_song(song_id, song_quality, artist_name, release_year):
     song_response = requests.get(f"{url}track/?id={song_id}&quality={song_quality}").json()
     song_path = f"../{artist_name}/{artist_name} - {song_response[0]['album']['title']} ({release_year})/"
 
-    if not os.path.exists(song_path):
-        os.makedirs(song_path)
+    if not os.path.exists(String_replace(song_path)):
+        os.makedirs(String_replace(song_path))
     else:
         "Something doesnt work with the path"
     
@@ -37,22 +44,22 @@ def Download_cover(cover_code, cover_path):
         c.write(cover_response.content)
 
 
-os.system("clear")
+os.system("cls")
 while True:
     print("Download Artist ONLY(fuck you)\n")
-    artist_name = input("Artist Name: ")
+    artist_name = String_replace(input("Artist Name: "))
     artist_response = requests.get(f"{url}search/?a={artist_name}").json()
 
     for artist in artist_response[0]['artists']['items']:
         print(f"{artist['id']} - {artist['name']}")
     
     artist_id = input("Artist ID: ")
-    os.system("clear")
+    os.system("cls")
     artist_id_response = requests.get(f"{url}search/?f={artist_id}").json()
 
     for album in artist_id_response[1]:
         print(f"{album['id']}. {album['album']['title']} - {album['title']}")
         artist_name = artist_id_response[0]['rows'][0]['modules'][0]['pagedList']['items'][0]['artists'][0]['name']
         release_year = album['album']['releaseDate'].split("-")[0]
-        Download_song(album['id'], "HI_RES_LOSSLESS", artist_name, release_year)
+        Download_song(album['id'], "LOW", artist_name, release_year)
         
